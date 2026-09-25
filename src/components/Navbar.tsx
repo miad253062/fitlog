ceff0e5bed17485267f9eb540fc8f856c652dde0
miad253062 <miadac681@gmail.com>
2026-09-25 11:47:47 +0600

    Navbar padding decreased

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,10 +19,10 @@ function Navbar() {
           </Link>
 
           <div className="flex gap-2">
-            <Link href={"/"} className={`px-3 py-2 font-bold rounded-3xl ${pathname === '/' && 'text-[#ccff00] bg-[#1b4112]'}`}>Workouts</Link>
+            <Link href={"/"} className={`px-2 py-1 md:px-3 md:py-2 font-bold rounded-3xl ${pathname === '/' && 'text-[#ccff00] bg-[#1b4112]'}`}>Workouts</Link>
             <Link
               href={"/my-plan"}
-              className={`px-3 py-2 font-bold rounded-3xl ${pathname ==='/my-plan' && 'text-[#ccff00] bg-[#1b4112]'}`}
+              className={`px-2 py-1 md:px-3 md:py-2 font-bold rounded-3xl ${pathname ==='/my-plan' && 'text-[#ccff00] bg-[#1b4112]'}`}
             >
               My Plan
             </Link>
